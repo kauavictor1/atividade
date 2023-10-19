fdc830551d48cc5acde647fec74fc02940e1ba72
kauavictor1 <144855462+kauavictor1@users.noreply.github.com>
2023-10-19 14:51:46 -0300

    Z

---
+++ Z.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+char sexo;
+int maioridade;
+int idade;
+float renda;
+float salario;
+int opicao;
+char F;
+char f;
+char M;
+char m;
+int contado;
+int contadorMulher;
+do
+{
+    
+printf("digite seu sexo F para Feminino e M para Masculino: ");
+scanf("%s",&sexo);
+
+printf("digite sua idade: ");
+scanf("%d",&idade);
+
+printf("digite seu salario: ");
+scanf("%d",&salario);
+
+printf("============================================= \n");
+printf("digite 1 para adiciona mais uma pessoa: \n");
+printf("digite 2 par finalizar e esibir dados: \n");
+printf("digite sua opicao: ");
+scanf("%d",&opicao);
+printf("=============================================\n");
+
+maioridade = maioridade > idade;
+
+
+}
+while (opicao == 1);
+{
+if(sexo == 'F' && salario . 5.000) {
+contadorMulher++;
+    
+} 
+
+
+    system("cls");
+
+    printf("sexo: %s \n", sexo);
+    printf("idade: %d \n", idade);
+    printf("salario: %d \n",salario);
+}
+
+
+
+    return 0;
+}
