6a357b723ad8e67d5fe089676ae1a15662dcbe06
kauavictor1 <144855462+kauavictor1@users.noreply.github.com>
2023-10-19 16:41:42 -0300

    Add files via upload

---
+++ AB.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+char login [200];
+char senha [200];
+char loginSalvo [200] = "kaua";
+char senhaSalva [200] = "victor";
+
+do
+{
+printf("digite o login: ");
+gets(login);
+
+printf("digite a senha: ");
+gets(senha);
+
+if(strcmp(login, loginSalvo) == 0 && strcmp(senha, senhaSalva) == 0){
+    printf("bem vindo !");
+}else{
+    printf("acesso negado !");
+}
+}
+while (login > loginSalvo || senha > senhaSalva)
+{
+    printf("aaaaaaaaa");
+}
+
+    return 0;
+}
